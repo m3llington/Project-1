ad8b12f78b3d36d5a84bdea6a7a2a0c807010695
Blake Bovaird <cblakebovaird@gmail.com>
2019-07-24 19:37:14 -0400

    adding api javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
 function displayBreweries(){
-    // var brewery = [];
+    
     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=orlando";
 
     $.ajax({
@@ -13,18 +13,9 @@ function displayBreweries(){
 }
 displayBreweries();
 
-// function displayRestaurants (){
-//     var restaurant = [];
-//     var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=orlando?api_key=34d46252ce0812f6608d380b60411f53";
-
-//     $.ajax({
-//         url: queryURL,
-//         method : "GET"
-//     })
-// }
 
 function displayEvents(){
-    // var event = [];
+    
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=TSEGa9L3UMUnkG8jrVBHViun6NdHepmA&postalCode=32803";
 
     $.ajax({
@@ -35,10 +26,7 @@ function displayEvents(){
         for (var i = 0; i < response._embedded.events.length ; i++){
             var event = response._embedded.events;
         console.log (event[0].dates.start.dateTime); 
-        }
-       
-
-    
+        }    
     });
 }
 
