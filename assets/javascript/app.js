a54803a733f07b7a6541f2758d448beb4045a141
Blake Bovaird <cblakebovaird@gmail.com>
2019-07-24 21:31:45 -0400

    added javascript to generate tables and pull brewery information

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,53 @@
 
 function displayBreweries(){
     
-    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=orlando";
+    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city +"";
+    var city = $("#event").val();
 
     $.ajax({
         url: queryURL,
         method : "GET"
      }).then(function(response) {
-        //  console.log(response);
-         console.log(response[0].name);
+         console.log(response);
 
         var breweries = response;
          for (var i = 0; i < breweries.length; i++){
-            console.log(breweries[i].name);
+            //  Creating variables to hold brewerie information
+            var brewery = {
+                 name : breweries[i].name,
+                 postalCode : breweries[i].postal_code,
+                 type : breweries[i].brewery_type,
+                 city : breweries[i].city,
+                 phone : breweries[i].phone,
+                 state : breweries[i].state,
+                 street : breweries[i].street,
+                 url : breweries[i].website_url
+            };
+
+         
+           
+            console.log(brewery);
+
+            var tr = $("<tr>");
+
+            var tdName = $("<td>").text(brewery.name);
+            var tdPostalCode = $("<td>").text(brewery.postalCode);
+            var tdType = $("<td>").text(brewery.type);
+            var tdCity = $("<td>").text(brewery.city);
+            var tdPhone = $("<td>").text(brewery.phone);
+            var tdState = $("<td>").text(brewery.state);
+            var tdStreet = $("<td>").text(brewery.street);
+            var tdUrl = $("<td>").text(brewery.url);
             
-            $(".breweries").text(breweries[i].name);
+
+            tr.append(tdName).append(tdType).append(tdPhone).append(tdStreet).append(tdCity).append(tdState).append(tdPostalCode).append(tdUrl);
+
+            $(".breweries").append(tr);
+            // $(".breweries").append(brewery.name);
+            
+
+            // $(".breweries").append(breweries[i].name + " ");
+            // $(".breweries").append(breweries[i].postal_code);
          }
 
      });
@@ -41,5 +74,4 @@ function displayEvents(){
 displayEvents();
 
 // console.log("works");
-console.log("works");
 
