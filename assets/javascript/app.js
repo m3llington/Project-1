ed22eb5c3f0f35830877fe3cdf1fffa48543219b
Ricen <alejandrodgarcia71@gmail.com>
2019-07-24 21:09:10 -0400

    light changes to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,32 @@
 
-function displayBreweries() {
-
+function displayBreweries(){
+    
     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=orlando";
 
     $.ajax({
         url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+        method : "GET"
+     }).then(function(response) {
+         console.log(response);
 
-    });
+     });
 }
 displayBreweries();
 
 
-function displayEvents() {
-
+function displayEvents(){
+    
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=TSEGa9L3UMUnkG8jrVBHViun6NdHepmA&postalCode=32803";
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+        url : queryURL,
+        method : "GET"
+    }).then(function(response) {
         console.log(response);
-        for (var i = 0; i < response._embedded.events.length; i++) {
+        for (var i = 0; i < response._embedded.events.length ; i++){
             var event = response._embedded.events;
-            console.log(event[0].dates.start.dateTime);
-        }
+        console.log (event[0].dates.start.dateTime); 
+        }    
     });
 }
 
