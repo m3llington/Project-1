678c373dffac81855f79bef4e884eecd33cd1398
Ricen <alejandrodgarcia71@gmail.com>
2019-07-27 09:20:06 -0400

    fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,53 @@
 
-function displayBreweries(){
-    
+var zip = $("#zip").val().trim()
+var address = $("#address").val().trim()
+var events = $("#event").val().trim()
+
+$("#search").on("click", function () {
+    event.preventDefault()
+    if (zip.length > 0 || address.length > 0 || events.length > 0) {
+        console.log("you have something");
+    } else {
+        console.log("its blank");
+    }
+})
+
+
+
+
+function displayBreweries() {
+
     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=orlando";
 
     $.ajax({
         url: queryURL,
-        method : "GET"
-     }).then(function(response) {
-         console.log(response);
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
-     });
+    });
 }
 displayBreweries();
 
 
-function displayEvents(){
-    
+function displayEvents() {
+
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=TSEGa9L3UMUnkG8jrVBHViun6NdHepmA&postalCode=32803";
 
     $.ajax({
-        url : queryURL,
-        method : "GET"
-    }).then(function(response) {
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-        for (var i = 0; i < response._embedded.events.length ; i++){
+        for (var i = 0; i < response._embedded.events.length; i++) {
             var event = response._embedded.events;
-        console.log (event[0].dates.start.dateTime); 
-        }    
+            console.log(event[0].dates.start.dateTime);
+        }
     });
 }
 
 displayEvents();
 
 
-console.log("works");
+//console.log("works");
 
