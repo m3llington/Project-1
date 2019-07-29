af0c5a893e8629a6405b0272bb7db52739552e3d
Blake Bovaird <cblakebovaird@gmail.com>
2019-07-29 18:23:47 -0400

    fixed state and postal code search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,10 @@
 function displayBreweries(){
     console.log("test");
     
-  $("#Event").on("submit", function(event){
+  $(".Info").on("submit", function(event){
     event.preventDefault();
     console.log("test2");
-    var city = $("#event").val().trim();
+    var city = $("#city").val().trim();
     //searches for breweries by city, and limit it to showing 5
     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city +"&per_page=5";
 
@@ -37,13 +37,13 @@ function displayBreweries(){
          
            
             console.log(brewery);
-
+            
             var tr = $("<tr>");
 
             var tdName = $("<td>").text(brewery.name);
             var tdPostalCode = $("<td>").text(brewery.postalCode);
             // create code to get 'type' capitalized
-            var tdType = $("<td>").text(brewery.type);
+            var tdType = $("<td>").text(brewery.type.toUpperCase());
             var tdCity = $("<td>").text(brewery.city);
             var tdPhone = $("<td>").text(brewery.phone);
             var tdState = $("<td>").text(brewery.state);
@@ -78,9 +78,19 @@ function displayBreweries(){
 // 2. Create an on-click function for the events to display, then create a second on-click function for the zip-code of each displayed event that populates a second table below that lists the breweries in order of closest proximity.
 
 function displayEvents(){
-    var city = "orlando";
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=TSEGa9L3UMUnkG8jrVBHViun6NdHepmA&city=" + city + "";
+
+    $(".Info").on("submit", function(event){
+        event.preventDefault();
+
+        var city = $("#city").val().trim();
+        var state = $("#state").val().trim();
+        var type = $("#type").val().trim();
+        var zip = $("#zip").val().trim();
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=TSEGa9L3UMUnkG8jrVBHViun6NdHepmA&city=" + city + "&stateCode=" + state + "&postalCode=" + zip+ "&size=5";
     console.log("displayevents");
+
+    $(".events").text("");
+
     $.ajax({
         url : queryURL,
         method : "GET"
@@ -91,13 +101,53 @@ function displayEvents(){
         
 
         for (var i = 0; i < eventsArray.length ; i++){
+            var event = {
+                name : eventsArray[i].name,
+                type : eventsArray[i].classifications[0].segment.name,
+                street : eventsArray[i]._embedded.venues[0].address.line1,
+                city: eventsArray[i]._embedded.venues[0].city.name,
+                state: eventsArray[i]._embedded.venues[0].state.name,
+                zip: eventsArray[i]._embedded.venues[0].postalCode,
+                url : eventsArray[i]._embedded.venues[0].url
+            }
             
-           console.log(eventsArray[i].name);
+           console.log(eventsArray[i]);
+        // console.log(eventsArray[i].classifications[0].segment.name);
+       
+        var tr = $("<tr>");
+
+            var tdName = $("<td>").text(event.name);
+            var tdPostalCode = $("<td>").text(event.zip);
+            // create code to get 'type' capitalized
+            var tdType = $("<td>").text(event.type.toUpperCase());
+            var tdCity = $("<td>").text(event.city);
+            var tdState = $("<td>").text(event.state);
+            var tdStreet = $("<td>").text(event.street);
+            var tdUrl = $("<td>").text(event.url);
+
+            tr.append(tdName).append(tdType).append(tdStreet).append(tdCity).append(tdState).append(tdPostalCode).append(tdUrl);
+
+            $(".events").append(tr);
+
+            // Clears the text field 
+            $("#city").val("");
+            $("#state").val("");
+            $("#zip").val("");
+            $("#type").val("");
+            $("#start").val("");
+            $("#end").val("");
         }    
+    });
     });
 }
 displayBreweries();
 displayEvents();
 
 // console.log("works");
+
+
+// bonus bells and whistles
+// create a search box for type to filter between music, sports, etc.
+// create code to list breweries in order of proximity to clicked event
+
 
